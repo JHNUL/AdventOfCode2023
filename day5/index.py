6d9461df4188c50990ec0a1bc202fabcf3642625
Juhani Riisiö <juhani.riisio@gmail.com>
2023-12-03 08:47:50 +0200

    modify driver code

--- day5/index.py
+++ day5/index.py
@@ -1,9 +1,24 @@
-def solution(x: str):
+def solution_part1(line: str):
     pass
 
 
+def solution_part2(line: str):
+    pass
+
+
+def solution(path: str, part=1):
+    with open(path) as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            if part == 1:
+                res = solution_part1(line)
+            else:
+                res = solution_part2(line)
+
 
 if __name__ == "__main__":
-    result = solution("x")
-    print(result)
-
+    res1 = solution("input.txt", 1)
+    res2 = solution("input.txt", 2)
+    print(res1, res2)
