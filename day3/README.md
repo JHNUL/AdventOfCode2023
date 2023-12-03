711b9aa85b201eba280e6ab1a0f2cedde0713600
Juhani Riisiö <juhani.riisio@gmail.com>
2023-12-03 10:44:12 +0200

    day3

--- day3/README.md
+++ day3/README.md
@@ -1,6 +1,14 @@
 # Day 3 solution
 
+https://adventofcode.com/2023/day/3
+
 ### Part 1
 
+To find symbol-adjacent numbers we first read the input into a 2D matrix and populate the cells of every digit with the entire number that the digit is a part of. Also the coordinates of the first digit of the number is saved to avoid summing up the same number more than once later on. After this, doing an adjacency search of the matrix is simpler.
+
+![day3](day3.png)
+
 ### Part 2
+
+Same idea of an adjacency search but this time only look for "*" with two adjacent numbers and save those.
 
--- day3/index.py
+++ day3/index.py
@@ -1,24 +1,71 @@
-def solution_part1(line: str):
-    pass
+import re
 
 
-def solution_part2(line: str):
-    pass
+def is_adjacent(matrix, y, x):
+    for row in range(max(y - 1, 0), min(y+2, len(matrix))):
+        for col in range(max(x - 1, 0), min(x + 2, len(matrix[row]))):
+            if not re.match("\.|\d", matrix[row][col]):
+                return True
+    return False
 
 
-def solution(path: str, part=1):
+def get_gear_ratio(matrix, y, x):
+    s = set()
+    for row in range(max(y - 1, 0), min(y+2, len(matrix))):
+        for col in range(max(x - 1, 0), min(x + 2, len(matrix[row]))):
+            if re.match("\d.*", matrix[row][col]):
+                s.add(matrix[row][col])
+    if len(s) != 2:
+        return 0
+    l = [int(entry.split(" ")[0]) for entry in s]
+    return l[0]*l[1]
+
+
+def solution_part1(matrix: list):
+    s = set()
+    for y, row in enumerate(matrix):
+        for x, col in enumerate(row):
+            if re.match("\d", matrix[y][x]) and is_adjacent(matrix, y, x):
+                s.add(matrix[y][x])
+    return sum([int(entry.split(" ")[0]) for entry in s])
+
+
+def solution_part2(matrix: list):
+    s = 0
+    for y, row in enumerate(matrix):
+        for x, col in enumerate(row):
+            if re.match("\*", matrix[y][x]):
+                s += get_gear_ratio(matrix, y, x)
+    return s
+
+
+def create_matrix(path: str):
     with open(path) as f:
-        while True:
-            line = f.readline()
-            if not line:
-                break
-            if part == 1:
-                res = solution_part1(line)
-            else:
-                res = solution_part2(line)
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            l = []
+            the_line = line.strip()
+            j = 0
+            while j < len(the_line):
+                symbol = the_line[j]
+                new_symbol = symbol
+                if re.match("\d", symbol):
+                    # iterate forward until we have the whole number
+                    m = j + 1
+                    while m < len(the_line) and re.match("\d", the_line[m]):
+                        new_symbol += the_line[m]
+                        m += 1
+                    j = m - 1
+                    new_symbol += f" ({i},{j})"
+                for k in range(len(new_symbol.split(" ")[0])):
+                    l.append(new_symbol)
+                j += 1
+            lines[i] = l
+        return lines
 
 
 if __name__ == "__main__":
-    res1 = solution("input.txt", 1)
-    res2 = solution("input.txt", 2)
+    m = create_matrix("input.txt")
+    res1 = solution_part1(m)
+    res2 = solution_part2(m)
     print(res1, res2)
