7136c02fb5f32819e0532c8af25c4d970a186f86
Juhani Riisiö <juhani.riisio@gmail.com>
2023-12-05 20:22:06 +0200

    day5 part 1

--- day5/README.md
+++ day5/README.md
@@ -1,6 +1,15 @@
 # Day 5 solution
 
+https://adventofcode.com/2023/day/5
+
 ### Part 1
+
+Iterate througn maps looking at the ranges for destinations. If destination cannot be mapped, return the input number. I parsed the maps to a linked list so it does not matter how many maps there are or their names, so long as they are in the correct order of iteration in the input.
+
+![part 1](day5p1.png)
 
 ### Part 2
 
+"I didn't expect to tap out this early..."
+
+Well gosh darn it. It seems that the solution from the first part does not really help with the second part. How does one do a search with an initial range without iterating through each number in said range... need to get back to this one!
--- day5/index.py
+++ day5/index.py
@@ -1,8 +1,55 @@
-def solution_part1(n: list):
-    pass
+import re
 
 
-def solution_part2(n: list):
+class Node:
+    def __init__(self, name: str):
+        self.name = name
+        self.ranges = []
+        self.nxt = None
+
+    def add_range(self, range):
+        nums = [int(x) for x in range.replace("  ", " ").strip().split(" ")]
+        self.ranges.append(nums)
+
+    def get_dest(self, n):
+        for rng in self.ranges:
+            if rng[1] <= n and n <= rng[1]+rng[2]-1:
+                return rng[0]+(n-rng[1])
+        return n
+
+    def __repr__(self) -> str:
+        return f"{self.ranges}"
+
+
+def parse_maps(l: list):
+    link = None
+    head = None
+    for line in l:
+        if re.match(".+-.+-.+map:", line):
+            new = Node(line.split(" ")[0].strip())
+            if link is not None:
+                link.nxt = new
+            else:
+                head = new
+            link = new
+        elif re.match("\d+ \d+ \d+", line):
+            link.add_range(line)
+    return head
+
+
+def solution_part1(head, seeds):
+    res = float("inf")
+    for seed in seeds:
+        node = head
+        num = seed
+        while node is not None:
+            num = node.get_dest(num)
+            node = node.nxt
+        res = min(res, num)
+    return res
+
+
+def solution_part2(first_row: str):
     pass
 
 
@@ -22,6 +69,7 @@ def read_input(path: str, as_matrix=False):
 
 if __name__ == "__main__":
     n = read_input("input.txt")
-    res1 = solution_part1(n)
-    res2 = solution_part2(n)
-    print(res1, res2)
+    node = parse_maps(n)
+    seeds = [int(x) for x in n[0].split(":")[1].strip().split(" ")]
+    res1 = solution_part1(node, seeds)
+    print(res1)
